a59c5fe392b32445419f254cef35b5fffa85f2c3
Tri Le <dsmingtri1980@gmail.com>
2024-05-04 22:58:32 -0400

    Fixed typo in import of listPage

--- client/src/routes/listPage/listPage.jsx
+++ client/src/routes/listPage/listPage.jsx
@@ -2,7 +2,7 @@ import { listData } from "../../lib/dummydata";
 import "./listPage.scss";
 import Card from "../../components/card/Card"
 import Map from "../../components/map/Map";
-import FilterListing from "../../components/filterListing/FilterListing";
+import FilterListing from "../../components/FilterListing/FilterListing";
 
 function ListPage() {
     const data = listData;
