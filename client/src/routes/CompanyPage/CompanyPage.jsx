7ea65b50b4373a91360ee066fa581d9d70116d85
aefehr <fehr.allie@gmail.com>
2024-05-05 09:59:41 -0400

    add company indicator in millions

--- client/src/routes/CompanyPage/CompanyPage.jsx
+++ client/src/routes/CompanyPage/CompanyPage.jsx
@@ -32,8 +32,8 @@ function CompanyPage() {
                         </Box>
                         <h3>{companyData.city.charAt(0).toUpperCase() + companyData.city.slice(1)}, {companyData.state.charAt(0).toUpperCase() + companyData.state.slice(1)}</h3>
                         <br />
-                        <p><strong>Revenue:</strong> ${companyData.revenue}</p>
-                        <p><strong>Profit:</strong> ${companyData.profit}</p>
+                        <p><strong>Revenue:</strong> ${companyData.revenue} million</p>
+                        <p><strong>Profit:</strong> ${companyData.profit} million</p>
                         <p><strong>Number of Employees:</strong> {companyData.num_of_employees}</p>
                         <p><strong>Sector:</strong> {companyData.sector}</p>
                         <p><strong>Newcomer:</strong> {companyData.newcomer}</p>
@@ -41,7 +41,7 @@ function CompanyPage() {
                         <p><strong>CEO Woman:</strong> {companyData.ceo_woman}</p>
                         <p><strong>Website:</strong> <a href={`//${companyData.website}`}>{companyData.website}</a></p>
                         <p><strong>Ticker:</strong> {companyData.ticker}</p>
-                        <p><strong>Market Cap:</strong> ${companyData.market_cap}</p>
+                        <p><strong>Market Cap:</strong> ${companyData.market_cap} million</p>
                     </Box>
                 ) : (
                     <p>Loading...</p>
